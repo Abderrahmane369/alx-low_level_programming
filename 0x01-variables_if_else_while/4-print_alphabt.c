a664224a27ff41ee93aabfb9ba1b94d03634cdf5
Abderrahmane369 <cszamasu@gmail.com>
2023-06-16 11:39:12 -0700

    rr

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ *
+ * Description: Soemthing
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+if (i == 16 || i == 4)
+{
+putchar(97 + i);
+}
+putchar('\n');
+return (0);
+}
+
