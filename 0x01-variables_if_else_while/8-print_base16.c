f9493a66f32685673bd412706a3eb912a9b682f9
Abderrahmane369 <cszamasu@gmail.com>
2023-06-16 12:50:34 -0700

    ee

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ *
+ * Description: Soemthing
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + 48);
+}
+for (i = 0; i <= 5; i++)
+{
+putchar(i + 97);
+}
+putchar('\n');
+return (0);
+}
