381d5e4bf7ad58c8104ad0185fdd72bfe7537abe
Abderrahmane369 <cszamasu@gmail.com>
2023-06-16 11:17:37 -0700

    ab

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ *
+ * Description: Soemthing
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i < 26; i++)
+{
+putchar(97 + i);
+}
+
+return 0;
+}
+
