0e13584a20be02650e11df5af99fa92c510c3da3
Abderrahmane369 <cszamasu@gmail.com>
2023-06-16 13:27:58 -0700

    ee

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - something
+ *
+ * Description: Soemthing
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+{
+putchar(i + 48);
+putchar(',');
+}
+
+return (0);
+}
