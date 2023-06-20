31cc82d94a88327a923ca1d620b9ba4387825956
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 16:36:58 -0700

    zeae

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * times_table - hieheieei
+ */
+
+void times_table(int n)
+{
+int i, j;
+for (i = 0; i <= n; i++)
+{
+for (j = 0; j <= n; j++)
+{
+if (j != 0)
+printf("%2d", i * j);
+else
+printf("0");
+
+if (j != n)
+printf(", ");
+}
+printf("\n");
+}
+}
