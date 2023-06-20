4351cc3ac2048297fd3c753e936279ea5f050e81
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 08:20:56 -0700

    ea

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -6,14 +6,20 @@
  * Description: YeseaY
  * Return: 0 (Success)
  */
+void print_alphabet(void);
 
 int main(void)
+{
+print_alphabet();
+return (0);
+}
+
+void print_alphabet(void)
 {
 int i;
 for (i = 0; i < 26; i++)
 {
 putchar(97 + i);
 }
-putchar('\n');
-return (0);
+purchar('\n');
 }
