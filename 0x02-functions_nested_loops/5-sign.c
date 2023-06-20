236ea3c29e0adbf482270a0d3cdf93faff6e1830
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 11:53:54 -0700

    COMMIT

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include <ctype.h>
+#include "main.h"
+/**
+ * print_sign - it prints
+ * @n: EA SPORTS
+ * Return: 0 if not 1 one ok hh
+ */
+
+int print_sign(int n)
+{
+   if (n == 0) {
+      _putchar('0');
+      return (0);
+   }
+   else if (n > 0) {
+      _('+');
+      return (1);
+   }
+   else {
+      _putchar('-');
+      return (-1);
+   }
+}
