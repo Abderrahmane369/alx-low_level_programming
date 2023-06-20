533757ffc799d9a9987fe88d9de809baf79b5da7
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 13:59:12 -0700

    EE

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * jack_bauer - db hadi
+ */
+
+void jack_bauer(void)
+{
+int i, j;
+
+for (i = 0; i <= 23; i++)
+{
+for (j = 0; i <= 59; i++)
+{
+printf("%02d:%02d\n", i, j);
+}
+}
+}
