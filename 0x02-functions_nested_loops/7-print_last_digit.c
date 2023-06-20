f700c92af1642d89e100f6ab7e4c868186efd54e
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 12:09:59 -0700

    COMMIT

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * print_last_digit - hiya wahed lfunctionse
+ * @n: hiyac wlahrt fojnction
+ * Return: 0 ila kannt 1 hh
+ */
+
+int print_last_digit(int n)
+{
+return (n % 10);
+}
