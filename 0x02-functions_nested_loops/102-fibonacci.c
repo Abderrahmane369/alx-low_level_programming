bf4ed94abb5c1d22d6097710578ebc76c32a8da2
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 18:02:07 -0700

    eazeaea

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - ezazae
+ * Return: 0 if it
+ */
+
+int main() {
+
+for (int k = 2; k < 50; k++)
+{
+int i;
+long long a = 0;
+long long b = 1;
+long long sum = 0;
+
+for (i = 2; i <= k; i++)
+{
+sum = a + b;
+a = b;
+b = sum;
+}
+
+printf("%lld", sum);
+if (k != 49)
+printf(", ");
+}
+
+return 0;
+}
