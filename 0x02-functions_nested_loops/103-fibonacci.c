93f850cee0068495bc3fca4c99a91cc470f2a9df
Abderrahmane369 <cszamasu@gmail.com>
2023-06-20 18:26:14 -0700

    eazeaea

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,8 +7,29 @@
 
 int main(void)
 {
-int n = 4613732;
-printf("%d\n", n);
+int suM;
+
+for (int k = 2; k < 50; k++)
+{
+int i;
+unsigned long a = 0;
+unsigned long b = 1;
+unsigned long sum = 0;
+
+for (i = 2; i <= k; i++)
+{
+sum = a + b;
+a = b;
+b = sum;
+}
+if (sum > 4000000)
+   break;
+
+if (sum % 2 == 0)
+   suM += sum;
+
+}
+printf("%ld", suM);
 
 return (0);
 }
