ba822019d9a52c74720df8f6384dcbde2abd00cc
Abderrahmane369 <cszamasu@gmail.com>
2023-06-22 12:08:29 -0700

    COMMIT

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * main - mainmenu
+ * Return: 0 if 1
+ */
+
+int main(void)
+{
+for (a = 1; a < 100; a++)
+{
+if (a % 15 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (a % 5 == 0)
+{
+printf("Buzz ");
+}
+else if (a % 3 == 0)
+{
+printf("Fizz ");
+}
+else
+{
+printf("%d ", a);
+}
+}
+putchar('\n');
+
+return (0);
+}
