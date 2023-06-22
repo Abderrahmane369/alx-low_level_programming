4e8e3cc67b07986cb41f475e6588c0014e946311
Abderrahmane369 <cszamasu@gmail.com>
2023-06-22 11:40:50 -0700

    COMMIT

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,19 @@
+#include "main"
+
+/**
+ * print_line - iseaezaeza
+ * @n: eiaizeez
+ */
+void print_line(int n)
+{
+int l;
+if (n > 0)
+{
+for (l = 0; l < n; l++)
+{
+_putchar('_');
+}
+}
+
+_putchar('\n');
+}
