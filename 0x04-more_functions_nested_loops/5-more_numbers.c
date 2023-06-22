118eadaf3986988af9086f52e54f5a0dd80393da
Abderrahmane369 <cszamasu@gmail.com>
2023-06-22 11:32:58 -0700

    COMMIT

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,12 +11,11 @@ for (i = 0; i <= 9; i++)
 {
 for (a = 0; a <= 14; a++)
 {
-if (a / 9 != 0)
+if (a / 10 != 0)
 _putchar(a  / 9 + 48);
 
 _putchar(a % 10 + 48);
 }
-if (i != 9)
 _putchar('\n');
 }
 }
