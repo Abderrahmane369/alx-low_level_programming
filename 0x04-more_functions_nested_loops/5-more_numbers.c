03fe6406615589aa384025d7db18e859d0cb368b
Abderrahmane369 <cszamasu@gmail.com>
2023-06-22 11:24:03 -0700

    COMMIT

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * more_numbers - ok
+ */
+void more_numbers(void)
+{
+int i, a;
+
+for (i = 0; i <= 9; i++)
+{
+for (a = 0; a <= 14; a++)
+{
+if (a / 9 != 0)
+_putchar(a  / 9 + 48);
+
+_putchar(a % 10 + 48);
+}
+_putchar('\n');
+}
+}
