123c0aa8e039a0316589239b38b2d106ce86277f
Abderrahmane369 <cszamasu@gmail.com>
2023-06-26 10:31:25 -0700

    gita

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * print_rev - rezad
+ * @s: eeazea
+ */
+
+void print_rev(char *s)
+{
+int k;
+
+for (k = 0; k < strlen(s); k++)
+{
+putchar(*(s + strlen(s) - (1 + k));
+}
