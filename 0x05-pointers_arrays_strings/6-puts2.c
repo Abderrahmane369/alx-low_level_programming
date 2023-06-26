0f8361e9cd5f9726d6eb0e034c69f5f5e4a09837
Abderrahmane369 <cszamasu@gmail.com>
2023-06-26 16:45:53 -0700

    COMMIT

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * puts2 - iazep
+ * @str: eaeeza
+ */
+
+void puts2(char *str)
+{
+unsigned long int k;
+
+for (k = 0; k < strlen(s); k++)
+{
+if (k % 2 == 0)
+{
+putchar(s[k]);
+}
+}
+}
