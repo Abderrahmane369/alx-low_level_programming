216967fe75fdaf1570d7bc6895981c62431d409a
Abderrahmane369 <cszamasu@gmail.com>
2023-06-26 17:52:31 -0700

    COMMIT

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/*
+ * _atoi - aezpd
+ * @s: stgirng
+ * Return: 0 if not
+ */
+
+int _atoi(char *s)
+{
+if (s != "")
+return (atoi(s));
+else
+return (0);
+}
