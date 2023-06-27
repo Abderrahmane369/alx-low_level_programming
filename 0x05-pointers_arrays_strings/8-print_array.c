12c2220d30aaac3f0fe5c455899fdb08286b5b5d
Abderrahmane369 <cszamasu@gmail.com>
2023-06-26 17:16:07 -0700

    COMMIT

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * print_array - good
+ * @a: array
+ * @n: no
+ */
+
+int print_array(int *a, int n)
+{
+int k;
+
+for (k = 0; k < n; k++)
+{
+printf("%d", a[k]);
+if (k != (n - 1))
+printf(", ");
+}
+printf("\n");
+}
