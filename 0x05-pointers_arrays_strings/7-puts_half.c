d3c7523af2d5877c11d03f09d04dce934d80cf14
Abderrahmane369 <cszamasu@gmail.com>
2023-06-26 17:01:27 -0700

    COMMIT

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/** puts_half - eeaea
+ * @str: opo
+ */
+
+void puts_half(char *str)
+{
+unsigned long int k;
+
+for (k = 0; k < strlen(str); k++)
+{
+if (strlen(str) % 2 == 0)
+{
+if (k >= strlen(str) / 2)
+putchar(str[k]);
+}
+else {
+if (k >= (strlen(str) - 1) / 2)
+putchar(str[k]);
+}
+}
