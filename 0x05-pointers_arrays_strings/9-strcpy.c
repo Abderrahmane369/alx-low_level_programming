4ed14a4f89dd556a26c51f03498d39db12717a6f
Abderrahmane369 <cszamasu@gmail.com>
2023-06-26 17:31:52 -0700

    COMMIT

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcpy - sodqz
+ * @dest: ezae
+ * @stc :eazepaqd
+ * Return: arr and a
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+strcpy(dest, src);
+
+return (dest);
+}
