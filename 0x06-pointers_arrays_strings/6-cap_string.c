cd2ba0586a324ed390c53e51eab22bf5200151ae
Abderrahmane369 <cszamasu@gmail.com>
2023-06-29 11:03:32 -0700

    COMMIT

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * cap_string - ezaea
+ * @a: aaaaaaaaa
+ * Return: deistrub 0 and 1
+ */
+
+char *cap_string(char *a)
+{
+int k;
+
+for (k = 0; k < strlen(a); k++)
+{
+if (a[k] == ',' || a[k] == ';' || a[k] == '.' || a[k] == '!' || a[k] == '?' || a[k] == '"' || a[k] == '(' || a[k] == ')' || a[k] == '{' || a[k] == '}')
+{
+a[k + 2] = toupper(a[k + 2]);
+}
+}
+
+return (a);
+}
