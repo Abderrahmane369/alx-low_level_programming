3a2533d18c02cebf4ebd69f64f0736f4846499fa
Abderrahmane369 <cszamasu@gmail.com>
2023-06-29 05:39:33 -0700

    moocit

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strncpy - opopo
+ * @dest: desto
+ * @src: crs
+ * @n: n
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+strncpy(dest, src, n);
+
+return (dest);
+}
