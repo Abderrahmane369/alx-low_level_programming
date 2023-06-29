38d32212c19cfb8833bad6269fb4fbf778e59380
Abderrahmane369 <cszamasu@gmail.com>
2023-06-29 05:14:50 -0700

    moocit

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include <string.h>
+#include "main.h"
+
+/**
+ * _strcat - cat moew
+ * @dest: dust
+ * @src: tv
+ */
+
+char *_strcat(char *dest, char *src)
+{
+strcat(dest, src);
+
+return (dest);
+}
