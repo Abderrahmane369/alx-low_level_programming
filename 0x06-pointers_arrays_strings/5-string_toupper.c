e85b81704d6dc877c68805a680e703d2a14648ae
Abderrahmane369 <cszamasu@gmail.com>
2023-06-29 10:17:27 -0700

    ezae

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+#include <ctype.h>
+
+/**
+ * string_toupper - topeir
+ * @a: ss
+ * Return: t 1 AOR 0
+ */
+
+char *string_toupper(char *a)
+{
+int k;
+
+for (k = 0; k < strlen(a); k++)
+{
+a[k] = toupper(a[k]);
+}
+
+return (a);
+}
