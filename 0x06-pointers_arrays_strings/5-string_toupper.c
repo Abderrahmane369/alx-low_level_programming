0c3e3c1558318369757c23dba07462c477459ab4
Abderrahmane369 <cszamasu@gmail.com>
2023-06-29 10:37:27 -0700

    ezae

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,13 @@
  * Return: t 1 AOR 0
  */
 
-char *string_toupper(char *)
+char *string_toupper(char *a)
 {
 int k;
 
 for (k = 0; k < strlen(a); k++)
 {
-a[k] = toupper(a[k]);
+a[k] = 'a';
 }
 
 return (a);
