9f7cf3c9635e3fb9227581a066f44f0a9348e33f
Abderrahmane369 <cszamasu@gmail.com>
2023-07-03 05:46:07 -0700

    moaze

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,13 @@
+#include <string.h>
+#include "main.h"
+
+/** _strpbrk - hello
+ * @s: s
+ * @accept: apc
+ * Return: aa
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+return (strpbrk(s, accept);
+}
