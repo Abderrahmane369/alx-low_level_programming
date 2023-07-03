a8f92d70be1e4418ebc2a98209db565add70a694
Abderrahmane369 <cszamasu@gmail.com>
2023-07-03 04:59:06 -0700

    moaze

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memset - meme
+ * @s: s
+ * @b: b
+ * @n: n
+ * Return: a 1 a 0
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+memset(s, b, n);
+
+return (s);
+}
