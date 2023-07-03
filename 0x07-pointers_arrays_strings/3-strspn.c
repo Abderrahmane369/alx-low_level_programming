f859ff35bb590b124cd636f13d3d8d67e6ab3410
Abderrahmane369 <cszamasu@gmail.com>
2023-07-03 05:20:39 -0700

    moaze

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strspn - opi
+ * @s: osp
+ * @accept: accept
+ * Return: 0 1
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+return (strspn(s, accept);
+}
