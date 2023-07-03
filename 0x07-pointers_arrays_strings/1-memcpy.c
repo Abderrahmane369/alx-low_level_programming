ffe3f69aa85c7eedc250d5ba990ab11f1219b186
Abderrahmane369 <cszamasu@gmail.com>
2023-07-03 05:02:14 -0700

    moaze

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _memcpy - char
+ * @dest: d
+ * @src: sr
+ * @n: no
+ * Return: nul 0 1
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+memcpy(dest, src, n);
+
+return (dest);
+}
