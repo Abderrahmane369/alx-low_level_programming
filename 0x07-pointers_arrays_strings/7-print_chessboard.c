e56a29a7a0dccbf5e06715aefc87a7ae268cae82
Abderrahmane369 <cszamasu@gmail.com>
2023-07-03 06:25:20 -0700

    moaze

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * print_chessboard - omega
+ * @a: aaaa
+ */
+
+void print_chessboard(char (*a)[8])
+{
+int k, j;
+
+for (k = 0; k < 8; k++)
+{
+for (j = 0; j < 8, j++)
+{
+putchar(a[k][j]);
+}
+putchar('\n');
+}
+}
