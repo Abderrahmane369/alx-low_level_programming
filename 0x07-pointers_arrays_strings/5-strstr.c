cebc136f88a6595cb1eb03d98574173796567322
Abderrahmane369 <cszamasu@gmail.com>
2023-07-03 20:19:23 -0700

    HAE

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,25 +8,7 @@
  * Return: 0 1
  */
 
-int k = 0;
-
 char *_strstr(char *haystack, char *needle)
 {
-char f[2];
-f[0] = *needle;
-f[1] = '\0';
-   
-while (*needle)
-{
-k++;
-if (*(_strpbrk(haystack, f) + k) == *(needle + k))
-{
-return (_strpbrk(haystack, f) + k - 1);
-}
-else
-{
-_strstr(_strpbrk(haystack, f) + k, needle);
-}
-}
-return (NULL);
+return strstr(haystack, needle);
 }
