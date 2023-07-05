9824cda3d3dbcddcd29c0102b51b7ae4236a2162
Abderrahmane369 <cszamasu@gmail.com>
2023-07-05 11:26:42 -0700

    ezae

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,6 +3,7 @@
 /**
  * _puts_recursion - eazea
  * @s: op
+ * Return: O0
  */
 
 void _puts_recursion(char *s)
@@ -11,6 +12,11 @@ if (*s)
 {
 _putchar(*s);
    s++;
+}
+else
+{
+_putchar('\n');
+return;
 }
 _puts_recursion(s);
 }
