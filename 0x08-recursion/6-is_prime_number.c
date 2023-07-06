38de8d03fcd57eeaef2016b62e5533df5827a0b5
Abderrahmane369 <cszamasu@gmail.com>
2023-07-05 17:32:27 -0700

    COMMit

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * i_is_prime_number - ezaa
+ * @n: eza
+ * @i: ez
+ * Return: tur
+ */
+
+int i_is_prime_number(int n, int i)
+{
+if (i >= n)
+return (1);
+
+if (n % i == 0)
+return (0);
+
+return (i_is_prime_number(n, i + 1));
+}
+
+/**
+ * _is_prime_number - ezaa
+ * @n: eza
+ * Return: tur
+ */
+
+int _is_prime_number(int n)
+{
+if (n < 0)
+return (-1);
+
+if (n <= 1)
+return (0);
+
+return (i_is_prime_number(n, 0));
+
+}
