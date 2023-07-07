b2e730b6c85396b60dcdda4c4f6d3f76ba794fb5
Abderrahmane369 <cszamasu@gmail.com>
2023-07-07 10:33:23 -0700

    eae

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
  * main - eza
@@ -7,10 +8,12 @@
  * Return: 0
  */
 
-int main(int argc, int *argv[])
+int main(int argc, char *argv[])
 {
 if (argc != 1)
-printf("%d\n", argv[1] * argv[2]);
+{
+printf("%d\n", atoi(n0) * atoi(n1));
+}
 
 else
 printf("Error");
