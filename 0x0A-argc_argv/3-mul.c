cca33cac1a396b478dec4fff0db139d552704737
Abderrahmane369 <cszamasu@gmail.com>
2023-07-07 09:57:45 -0700

    eae

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - eza
+ * @argc: zae
+ * @argv: zae
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+if (argc != 1)
+printf("%d\n", argv[1] * argv[2]);
+
+else
+printf("Error");
+
+return (0);
+}
