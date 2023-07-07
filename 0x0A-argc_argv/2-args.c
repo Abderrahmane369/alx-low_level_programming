89e5517b30a79f8d21867c4f22e847d75f0bcedf
Abderrahmane369 <cszamasu@gmail.com>
2023-07-07 09:23:44 -0700

    mee

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - eza
+ * @argv: ze
+ * @argc: aze
+ * Return: ezaeae
+ */
+
+int main(int argc, char *argv[])
+{
+int k = 0;
+
+while (k < argc - 1)
+{
+printf("%s\n", argv[k]);
+
+k++
+}
+return (0);
+}
