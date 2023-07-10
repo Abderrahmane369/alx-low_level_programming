09abbc9982f660f4fa0477c42104fd71625035ba
Abderrahmane369 <cszamasu@gmail.com>
2023-07-10 12:03:19 -0700

    ea

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * char *_strdup(char *str) - eaea
+ * @str: aeeaea
+ * Return: zaea
+ */
+
+char *_strdup(char *str)
+{
+	char *strv;
+
+	if (str == NULL)
+		return (NULL);
+
+	strv = malloc(sizeof(strlen(str) * sizeof(char)));
+
+	if (strv == NULL)
+		return (NULL);
+
+	strv = str;
+
+	return (strv);
+}
+
