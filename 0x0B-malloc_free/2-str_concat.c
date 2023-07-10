4a75a47498129d556b60c02c01c8683bb3a3fc47
Abderrahmane369 <cszamasu@gmail.com>
2023-07-10 13:37:08 -0700

    COME

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * str_concat - ea
+ * @s1: o
+ * @s2: op
+ * Return: sdaez
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *strcc;
+
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+
+	strcc = malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (strcc == NULL)
+		return (NULL);
+
+	strcpy(strcc, strcat(s1, s2));
+
+	return (strcc);
+}
