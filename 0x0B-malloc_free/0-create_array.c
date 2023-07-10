fdee3f5dea094947e31fc80f7f62049d421555b3
Abderrahmane369 <cszamasu@gmail.com>
2023-07-10 11:24:45 -0700

    ea

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * create_array - eazeae
+ * @size: sez
+ * @c: zea
+ * Return: ezaea
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+		return (NULL);
+	else
+	{
+		char *carr;
+
+		carr = malloc(sizeof(char) * (size + 1));
+
+		int k;
+
+		for (k = 0; k < size; k++)
+		{
+			carr[k] = c;
+		}
+
+		carr[size + 1] = '\0';
+		
+		return (carr);
+	}
