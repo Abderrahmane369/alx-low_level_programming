71632d8a33c6b29de400654f886687c603a2b0e2
Abderrahmane369 <cszamasu@gmail.com>
2023-07-11 09:10:50 -0700

    ezaea

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,31 +12,31 @@
 
 char *argstostr(int ac, char **av)
 {
-        char *s;
-        int k;
-        unsigned int len = 0;
+char *s;
+int k;
+unsigned int len = 0;
 
-        if (ac == 0|| av == NULL)
-                return (NULL);
+if (ac == 0|| av == NULL)
+return (NULL);
 
-        for (k = 0; k < ac; k++)
-        {
-                len += strlen(av[k]);
-        }
+for (k = 0; k < ac; k++)
+{	
+len += strlen(av[k]);
+}
 
-        s = malloc(sizeof(char) * (len + ac));
+s = malloc(sizeof(char) * (len + ac + 1));
 
-        if (s == NULL)
-                return (NULL);
+if (s == NULL)
+return (NULL);
 
-        strcpy(s, av[0]);
+strcpy(s, av[0]);
 
-        for (k = 1; k < ac; k++)
-        {
-                strcat(s, "\n");
-                strcat(s, av[k]);
-        }
-        strcat(s, "\n");
+for (k = 1; k < ac; k++)
+{
+strcat(s, "\n");
+strcat(s, av[k]);
+}
+strcat(s, "\n");
 
-        return (s);
+return (s);
 }
