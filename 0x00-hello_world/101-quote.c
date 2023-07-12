8593a5ad014f4786a66babb08b1c997e7e95dce1
Abderrahmane369 <cszamasu@gmail.com>
2023-07-11 17:18:41 -0700

    aa

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+/**
+ * main - eae
+ * Return - 1
+ */
 
-int main()
+int main(void)
 {
 	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
-	return 0;
+	return (1);
 }
