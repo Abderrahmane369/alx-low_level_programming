d45a6a5738884e8f8e68348d0268fb977e939741
Abderrahmane369 <cszamasu@gmail.com>
2023-07-13 08:56:02 -0700

    eeea

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stddef.h>
+#include <stdlib.>
+
+/**
+ * malloc_checked - eazeaz
+ * @b: eea
+ * Return: ezae
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *a = malloc(b);
+
+	if (a == NULL)
+		return (98);
+
+	return (a);
+}	
