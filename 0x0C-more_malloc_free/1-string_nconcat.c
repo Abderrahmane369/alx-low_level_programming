6e0581b43a1ec6c73d4380e15196abb98c2f0284
Abderrahmane369 <cszamasu@gmail.com>
2023-07-13 17:42:48 -0700

    eza

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,29 +13,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-char *str1 = s1;
-char *str2 = s2;
-char *string;
-unsigned int cn = n;
 
-if (cn > strlen(str2))
-cn = strlen(str2);
+size_t s1_len = strlen(s1);
+size_t s2_len = strlen(s2);
+char *result;
 
-if (str1 == NULL)
-str1 = "";
+if (s1 == NULL)
+s1 = "";
 
-if (str2 == NULL)
-str2 = "";
+if (s2 == NULL)
+s2 = "";
 
-string = malloc(sizeof(char) * (strlen(str1) + cn + 1));
+if (n >= s2_len)
+n = s2_len;
 
-if (string == NULL)
-return (NULL);
+result = malloc(sizeof(char) * (s1_len + n + 1));
+if (result == NULL)
+return NULL;
 
+memcpy(result, s1, s1_len);
+memcpy(result + s1_len, s2, n);
+result[s1_len + n] = '\0';
 
-memcpy(string, str1, strlen(str1));
-memcpy(string + strlen(str1), str2, cn);
-string[strlen(str1) + cn] = '\0';
-
-return (string);
+return result;
 }
+
