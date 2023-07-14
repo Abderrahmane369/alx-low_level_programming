40863a30c388a12c14a586769d91b3c21099d806
Abderrahmane369 <cszamasu@gmail.com>
2023-07-13 18:11:58 -0700

    eza

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <main.h>
+#include <stddef.h>
+
+/**
+ * _calloc - ezaea
+ * @nmemb: eza
+ * @size: zae
+ * Return: ezae
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *s;
+	unsigned int k;
+
+	if (nmemb * size == 0)
+		return (NULL);
+
+	s = malloc(size * nmemb);
+
+	if (s == NULL)
+		return (NULL);
+
+	for (k = 0; k < nmemb; k++)
+	{
+		s[k] = 0
+	}
+
+	return (s);
+}
