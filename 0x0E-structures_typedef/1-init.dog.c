90af12bd0974da4d8fa12a82b6e24befb180002a
Abderrahmane369 <cszamasu@gmail.com>
2023-07-17 13:37:55 -0700

    COMMIT

---
+++ 0x0E-structures_typedef/1-init.dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog - eaz
+ * @d: eaa
+ * @name: aez
+ * @age: eaze
+ * @owner: azea
+ */
+
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
