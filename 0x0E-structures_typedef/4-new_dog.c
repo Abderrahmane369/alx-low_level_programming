7c8c686308e8b86c9a8d037fe019669322bc8689
Abderrahmane369 <cszamasu@gmail.com>
2023-07-17 18:52:30 -0700

    ezae

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -14,7 +14,7 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	char *n = strdup(name);
-	char *o = strdup(name);
+	char *o = strdup(owner);
 	dog_t *d = malloc(sizeof(dog_t));
 
 	if (d == NULL || o == NULL || n == NULL)
