14216e2d6bf12f0a62f817edfb7283727d694a3b
Abderrahmane369 <cszamasu@gmail.com>
2023-07-17 18:07:07 -0700

    ezae

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include "dog.h"
+
+/**
+ * new_dog - eaz
+ * @name: za
+ * @age: aez
+ * @owner: eaz
+ * Return: eza
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	char *n = name;
+	char *o = owner;
+
+	dog_t *d = malloc(sizeof(dog_t));
+
+	if (d == NULL)
+		return (NULL);
+
+	dog_t->name = n;
+	dog_t->owner = o;
+	dog_t->age = age;
+
+	return (d);
+}
