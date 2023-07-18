04c2576f2bddd9a4cbbc40d8ea9d64fd64bd60b2
Abderrahmane369 <cszamasu@gmail.com>
2023-07-17 18:23:10 -0700

    ezae

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 
 /**
  * new_dog - eaz
@@ -12,13 +13,15 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	char *n = name;
-	char *o = owner;
-
+	char *n = malloc(sizeof(name));
+	char *o = malloc(sizeof(owner));
 	dog_t *d = malloc(sizeof(dog_t));
 
-	if (d == NULL)
+	if (d == NULL || o == NULL || n == NULL)
 		return (NULL);
+
+	strcpy(n, name);
+	strcpy(o, owner);
 
 	if (n == NULL || o == NULL)
 		return (NULL);
