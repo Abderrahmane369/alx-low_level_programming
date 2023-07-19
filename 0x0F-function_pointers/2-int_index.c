3a2ee7299abeaac33e4e739ad35c84bd52347fdd
Abderrahmane369 <cszamasu@gmail.com>
2023-07-19 11:26:58 -0700

    ezae

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,24 +1,28 @@
 #include "function_pointers.h"
 
 /**
- * int_index - ozssdks^dk
+ * int_index - ozssdksdk
  * @array: ezaezae
  * @size: ezaea
  * @cmp: eaz
+ * Return: ZEZ
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	int k;
 
-	if (size <= 0)
-		return (-1);
-
-	for (k = 0; k < size; k++)
+	if (array && cmp)
 	{
-		if (cmp(array[k]))
-			return (k);
-	}
+		if (size <= 0)
+			return (-1);
 
-	return (-1);
+		for (k = 0; k < size; k++)
+		{
+			if (cmp(array[k]))
+				return (k);
+		}
+
+		return (-1);
+	}
 }
