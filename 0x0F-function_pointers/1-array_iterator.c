cc2b9ea58e71972c12a54e74f2dde583fbaf58e3
Abderrahmane369 <cszamasu@gmail.com>
2023-07-19 10:22:33 -0700

    COMIT

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - opopp
+ * @array: eaze
+ * @size: ezaea
+ * @action: eza
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t k;
+
+	if (array && action)
+	{
+
+		for (k = 0; k < size_t; k++)
+			action(array[k]);
+	}
+}
+
