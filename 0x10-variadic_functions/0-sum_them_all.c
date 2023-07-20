ea2329bfbe3647eeada3cc2c402a24030eccd36b
Abderrahmane369 <cszamasu@gmail.com>
2023-07-20 11:24:28 -0700

    zae

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,5 +3,26 @@
 
 /**
  * print_sum - eza
- * @n
+ * @n: eaz
+ * @...: zeaea
+ * Return: to home
  */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	const unsigned int k;
+	int sum = 0;
+
+	if (n == 0)
+		return (0);
+
+	va_start(args, n);
+
+	for (k = 0; k < n; k ++)
+	{
+		s += va_arg(args, int);
+	}
+
+	return (s);
+}
