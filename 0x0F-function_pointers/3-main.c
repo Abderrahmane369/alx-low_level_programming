371e6773be17c26e9388506b2e46d1b7199273d5
Abderrahmane369 <cszamasu@gmail.com>
2023-07-19 17:15:57 -0700

    zeae

---
+++ 0x0F-function_pointers/3-main.c
@@ -0,0 +1,38 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * main - eza
+ * @argc: zae
+ * @argv: eza
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int (*f)(int, int) = get_op_func(argv[2]);
+	int result = f(atoi(argv[1]), atoi(argv[3]));
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (f == NULL)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+        if (strcmp(argv[2], "+") != 0 && argv[3] == 0)
+        {
+                printf("Error\n");
+                exit(100);
+        }
+
+	printf("%d\n", result);
+
+	return (0);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,61 @@
+#include "3-calc.h"
+
+/**
+ * op_add - aeaea
+ * @a: eaea
+ * @b: eaea
+ * Return: zaeae
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - aeaea
+ * @a: eaea
+ * @b: eaea
+ * Return: zaeae
+ */
+
+int op_sub(int a, int b)
+{
+        return (a - b);
+}
+
+/**
+ * op_mul - aeaea
+ * @a: eaea
+ * @b: eaea
+ * Return: zaeae
+ */
+
+int op_mul(int a, int b)
+{
+        return (a * b);
+}
+
+/**
+ * op_div - aeaea
+ * @a: eaea
+ * @b: eaea
+ * Return: zaeae
+ */
+
+int op_div(int a, int b)
+{
+        return (a / b);
+}
+
+/**
+ * op_mod - aeaea
+ * @a: eaea
+ * @b: eaea
+ * Return: zaeae
+ */
+
+int op_mod(int a, int b)
+{
+        return (a % b);
+}
