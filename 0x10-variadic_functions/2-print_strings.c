e0d3e3d98d31583a4a09a679c88deb8fac269461
Abderrahmane369 <cszamasu@gmail.com>
2023-07-20 13:31:46 -0700

    zae

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -2,7 +2,33 @@
 #include <stdarg.h>
 
 /**
- * print_string - eza
- * @n
+ * print_strings - eza
+ * @separator: eza
+ * @n: eza
+ * @..: eaaze
  */
 
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int k;
+char *strr;
+
+va_start(args, n);
+
+for (k = 0; k < n; k++)
+{
+strr = va_arg(args, char *);
+
+if (strr)
+printf("%s", strr);
+else
+printf("(nil)");
+
+if (separator && k < n - 1)
+printf("%s", separator);
+}
+va_end(args);
+
+printf("\n");
+}
