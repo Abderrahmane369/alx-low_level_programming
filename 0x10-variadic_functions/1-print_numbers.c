6c9f624b1cfcf730ba2ad36fc1c40c7a2334c6ad
Abderrahmane369 <cszamasu@gmail.com>
2023-07-20 11:38:18 -0700

    zae

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,7 +1,29 @@
-#include "variadic_functions.h"
 #include <stdarg.h>
+#include <stdio.h>
+#include <string.h>
 
 /**
  * print_numbers - eza
- * @n
+ * @n: eza
+ * @..: ezaa
+ * Return: ezazea
  */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list args;
+unsigned int k;
+
+va_start(args, n);
+
+for (k = 0; k < n; k++)
+{
+printf("%d", va_arg(args, int));
+
+if (separator && k < n - 1)
+printf("%s", separator);
+}
+printf("\n");
+
+va_end(args);
+}
