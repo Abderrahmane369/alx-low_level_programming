949d6d26a8d0da7838690fcdc3eb980e16646e65
Abderrahmane369 <cszamasu@gmail.com>
2023-07-19 18:50:08 -0700

    zeae

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -23,7 +23,10 @@ int (*get_op_func(char *s))(int, int)
 
 	i = 0;
 
-	while (i < 6)
+	if (!s)
+		return (NULL);
+
+	while (i < 5)
 	{
 		if (strcmp(s, ops[i].op) == 0)
 			return (ops[i].f);
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,24 +13,29 @@
 
 int main(int argc, char *argv[])
 {
-int (*f)(int, int) = get_op_func(argv[2]) != NULL ? get_op_func(argv[2]) : NULL;
-int result = f != NULL? f(atoi(argv[1]), atoi(argv[3])) : 0;
+int (*f)(int, int);
+int result;
 
 if (argc != 4)
 {
 printf("Error\n");
 exit(98);
 }
-if (f == NULL)
+if (get_op_func(argv[2]) == NULL)
 {
 printf("Error\n");
 exit(99);
 }
-if (strcmp(argv[2], "+") != 0 && argv[3] == 0)
+
+f = get_op_func(argv[2]);
+
+if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && strcmp(argv[3], "0") == 0)
 {
 printf("Error\n");
 exit(100);
 }
+
+result = f(atoi(argv[1]), atoi(argv[3]));
 
 printf("%d\n", result);
 
