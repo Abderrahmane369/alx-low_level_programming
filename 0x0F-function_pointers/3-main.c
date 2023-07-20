2a4a3711e2e746bc241cb75879ea71124c3676ad
Abderrahmane369 <cszamasu@gmail.com>
2023-07-19 19:24:53 -0700

    zeae

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -14,7 +14,8 @@
 int main(int argc, char *argv[])
 {
 int (*f)(int, int) = get_op_func(argv[2]);
-int result = result = f(atoi(argv[1]), atoi(argv[3]));
+int result;
+int is0 = (atoi(argv[3]) == 0);
 
 if (argc != 4)
 {
@@ -27,11 +28,13 @@ printf("Error\n");
 exit(99);
 }
 
-if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && strcmp(argv[3], "0") == 0)
+if (strcmp(argv[2], "+") != 0 && strcmp(argv[2], "-") != 0 && is0)
 {
 printf("Error\n");
 exit(100);
 }
+
+result = f(atoi(argv[1]), atoi(argv[3]));
 
 printf("%d\n", result);
 
