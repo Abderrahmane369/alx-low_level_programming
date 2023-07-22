76ffe14a215d105ff93ba0635118aa59ddd463e5
Abderrahmane369 <cszamasu@gmail.com>
2023-07-22 06:48:05 -0700

    zae

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,8 +1,58 @@
-#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <stdarg.h>
 
 /**
  * print_all -eza
- * @format:
+ * @format: ezae
+ * @...: ezaea
  */
 
+void print_all(const char * const format, ...)
+{
+va_list args;
+
+int k = 0;
+char *s;
+int sep = 0;
+
+va_start(args, format);
+
+while (format[k])
+{
+switch (format[k])
+{
+case 'c':
+printf("%s%c", (sep) ? " ," : "",va_arg(args, int));
+sep = 1;
+
+break;
+case 'i':
+printf("%s%d", (sep) ? " ," : "",va_arg(args, int));
+sep = 1;
+
+break;
+case 'f':
+printf("%s%s", (sep) ? " ," : "",va_arg(args, double));
+sep = 1;
+
+break;
+case 's':
+s = va_arg(args, char*);
+
+if (s)
+printf("%s%s", (sep) ? " ," : "", s);
+else
+printf("%s%s", (sep) ? " ," : "", "(nil)");
+
+sep = 1;
+}
+
+k++;
+}
+
+printf("\n");
+
+va_end(args);
+}
