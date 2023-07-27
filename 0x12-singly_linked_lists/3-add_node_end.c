64c3b57e445813f2c6f2280777cf0df8fa314996
Abderrahmane369 <cszamasu@gmail.com>
2023-07-27 15:26:03 -0700

    hello

---
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -0,0 +1,37 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+
+/**
+ * add_node_end - loo
+ * @h: eaze
+ * @str: ezae
+ * Return: loo
+ */
+
+list_t *add_node_end(list_t **head, const char *str)
+{
+	list_t *l = *head;
+
+	list_t *n = malloc(sizeof(list_t));
+
+	if (!n)
+		return (NULL);
+
+	n->str = strdup(str);
+	n->len = strlen(str);
+	n->next = NULL;
+
+	while (l && l->next)
+	{
+		l = l->next;
+	}
+
+	if (l)
+		l->next = n;
+	else
+		*head = n;
+
+	return (n);
+}
