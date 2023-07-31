52310138aadd0262a6d1eb0376261f19b28948a4
Abderrahmane369 <cszamasu@gmail.com>
2023-07-31 09:02:17 -0700

    HHHH

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "list.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * add_nodeint - ezaold
+ * @head: zea
+ * @n: eza
+ * Return: iezap
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new = malloc(sizeof(listint_t));
+
+	if (!new)
+		return (NULL);
+
+	new->n = n;
+	new->next = *head;
+
+	return (new);
+}
