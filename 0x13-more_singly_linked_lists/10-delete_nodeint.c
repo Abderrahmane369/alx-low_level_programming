ea0529a566dd0be62f863fe563dcc72e86cc0bc1
Abderrahmane369 <cszamasu@gmail.com>
2023-08-01 07:57:54 -0700

    opo

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * delete_nodeint_at_index - eza
+ * @head: eza
+ * @index:e za
+ * Return: zea
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *d, *c = *head;
+	unsigned int ci = 0;
+
+	while (c)
+	{
+		if (ci == index - 1)
+		{
+			d = c->next;
+			c->next = c->next->next;
+			free(d);
+			return (1);
+		}
+		else if (index == 0)
+		{
+			free(c);
+			return (1);
+		}
+		c = c->next;
+		ci++;
+	}
+
+	return (-1);
+}
+
--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -11,35 +11,31 @@
 
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-listint_t *new_node, *current = *head;
-unsigned int count = 0;
+	lisint_t *new, *c = *head;
+	unsigned int cI = 0;
 
-while (current != NULL && count < (idx - 1))
-{
-current = current->next;
-count++;
-}
+	while (c)
+	{
+		if (idx == 0)
+		{
+			new = malloc(sizeof(listint_t));
+			new->n = n;
+			new->next = c;
+			*head = new;
+			return (new);
+		}
+		else if (cI == idx - 1)
+		{
+			nw = malloc(sizeof(listint_t));
+			new->n = n;
+			new->next = c->next;
+			c->next = new;
+			return (new);
+		}
 
-if (current == NULL)
-return (NULL);
+		c = c->next;
+		cI++;
+	}
 
-new_node = malloc(sizeof(listint_t));
-if (new_node == NULL)
-return (NULL);
-
-new_node->n = n;
-new_node->next = NULL;
-
-if (idx == 0)
-{
-new_node->next = *head;
-*head = new_node;
-}
-else
-{
-new_node->next = current->next;
-current->next = new_node;
-}
-
-return (new_node);
+	return (NULL);
 }
