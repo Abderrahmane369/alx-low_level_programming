1488e1235a08dcabce4ea1bae3fe978de6d91e6b
Abderrahmane369 <cszamasu@gmail.com>
2023-07-31 19:08:59 -0700

    AAAAAAAAYOU

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * insert_nodeint_at_index - ezaaez
+ * @head: eza
+ * @idx: eza
+ * @n: eza
+ * Return: eza
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *c = *head;
+	unsigned int cIndex = 0;
+	listint_t *new;
+
+	while (c)
+	{
+		if (cIndex == idx)
+		{
+			new = malloc(sizeof(listint_t));
+
+			if (!new)
+				return (NULL);
+
+			new->n = n;
+			new->next = c->next;
+			c->next = new;
+
+			return (new);
+		}
+
+		c = c->next;
+		cIndex++;
+	}
+
+	return (NULL);
+}	
