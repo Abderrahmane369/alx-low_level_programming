c301a2dd9430ace2327049c41c2e2459cd235653
Abderrahmane369 <cszamasu@gmail.com>
2023-08-01 08:00:17 -0700

    opo

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -14,6 +14,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *new, *c = *head;
 	unsigned int cI = 0;
 
+	if (!head)
+		return (NULL);
+
 	while (c)
 	{
 		if (idx == 0)
