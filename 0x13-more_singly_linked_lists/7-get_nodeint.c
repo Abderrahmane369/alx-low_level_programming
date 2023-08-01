159a7838077c192a130e33f15fad380de59d3623
Abderrahmane369 <cszamasu@gmail.com>
2023-07-31 18:39:02 -0700

    AAAAAAAAYOU

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * get_nodeint_at_index - eza
+ * @head: eza
+ * @index: eza
+ * Return: eaz
+ */
+
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	listint_t *n = head;
+	unsigned int cI = 0;
+
+	while (n)
+	{
+		if (cI == index)
+			return (n);
+
+		n = n->next;
+		cI++;
+	}
+
+	return (NULL);
+}
