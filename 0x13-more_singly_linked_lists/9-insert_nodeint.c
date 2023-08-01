57a228d61a110680d0ad769cc1daa2a0acf27fb1
Abderrahmane369 <cszamasu@gmail.com>
2023-07-31 20:21:38 -0700

    AAAAAAAAYOU

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,24 +18,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (head == NULL)
 		return (NULL);
 	c = *head;
+
+	new = malloc(sizeof(listint_t));
+	if (!new)
+		return (NULL);
+	new->n = n;
+
 	while (c)
 	{
 		if (idx == 0)
 		{
-			new = malloc(sizeof(listint_t));
-			if (!new)
-				return (NULL);
-			new->n = n;
 			new->next = c;
 			*head = new;
 			return (new);
 		}
 		else if (cIndex == idx - 1)
 		{
-			new = malloc(sizeof(listint_t));
-			if (!new)
-				return (NULL);
-			new->n = n;
 			new->next = c->next;
 			c->next = new;
 			return (new);
@@ -43,5 +41,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 		c = c->next;
 		cIndex++;
 	}
+
+	free(new);
 	return (NULL);
 }
