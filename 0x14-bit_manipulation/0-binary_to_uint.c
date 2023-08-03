71bfee571f829ef02d66d95b8cd90e6831a125ad
Abderrahmane369 <cszamasu@gmail.com>
2023-08-03 08:14:35 -0700

    eza

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * binary_to_uint - za
+ * @b: eza
+ * Return: eza
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	const char *bn = b;
+	int k = 0;
+	unsigned int sum = 0;
+
+	if (!bn)
+		return (0);
+
+	while (*bn)
+	{
+		b++;
+	}
+
+	while (*bn)
+	{
+		if (*bn != '0' && *bn != '1')
+			return (0);
+
+		if (*bn == '1')
+		{
+			sum += (unsigned int)pow(2, k);
+		}
+
+		k++;
+		*bn++;
+	}
+
+	return (sum);
+}
