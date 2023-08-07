a3afed39b213f68ead0f879a5736eed5907a752a
Abderrahmane369 <cszamasu@gmail.com>
2023-08-07 16:41:28 -0700

    zae

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,7 +18,7 @@ int create_file(const char *filename, char *text_content)
 	if (!filename)
 		return (-1);
 
-	f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 543);
+	f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
 
 	if (f == -1)
 		return (-1);
