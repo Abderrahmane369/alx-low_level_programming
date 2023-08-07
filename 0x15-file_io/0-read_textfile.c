32a2828a1af77262295a5cd1747cf167938a9218
Abderrahmane369 <cszamasu@gmail.com>
2023-08-07 15:32:52 -0700

    zae

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
 #include <unistd.h>
-#include <fnct1>
+#include <fcntl.h>
 
 /**
  * read_textfile - eza
@@ -12,23 +12,32 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	FILE* f;
-	char buff[letters];
+	int f;
+	char *buff;
 	ssize_t rbytes, wbytes;
 
 	if (!filename)
 		return (0);
 
+	buff = malloc(sizeof(char) * letters);
+
+	if (!buff)
+		return (0);
+
 	f = open(filename, O_RDONLY);
 
 	if (f == -1)
+	{
+		free(buff);
 		return (0);
+	}
 
 	rbytes = read(f, buff, sizeof(buff));
 
 	if (rbytes == -1)
 	{
 		close(f);
+		free(buff);
 		return (0);
 	}
 
@@ -37,6 +46,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (wbytes != rbytes || wbytes == -1)
 	{
 		close(f);
+		free(buff);
 		return (0);
 	}
 
