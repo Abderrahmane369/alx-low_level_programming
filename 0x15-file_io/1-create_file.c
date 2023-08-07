8edc80ea7ff6c1b718791543ca9d893d09c7126c
Abderrahmane369 <cszamasu@gmail.com>
2023-08-07 16:24:12 -0700

    zae

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+/**
+ * create_file - eza
+ * @filename: eaz
+ * @text_content: eza
+ * Return: eza
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int f;
+	ssize_t wbytes;
+	char *txt = text_content ? text_content : "";
+	
+	if (!filename)
+		return (-1);
+
+	f = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 600);
+
+	if (f == -1)
+		return (-1);
+
+	while (*txt) {
+		if (write(f, txt, 1) == -1)
+		{
+			close(f);
+			return (-1);
+		}
+
+		txt++;
+	}
+
+	close(f);
+	return (1);
+}
