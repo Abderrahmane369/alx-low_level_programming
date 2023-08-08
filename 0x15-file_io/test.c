c135bed13fbb48eef6c485394e025b91a09ec768
Abderrahmane369 <cszamasu@gmail.com>
2023-08-07 17:19:56 -0700

    zae

--- 0x15-file_io/test.c
+++ 0x15-file_io/test.c
@@ -7,6 +7,7 @@
  *
  * Return: Always 0.
  */
+
 int main(int ac, char **av)
 {
     int res;
@@ -16,7 +17,7 @@ int main(int ac, char **av)
         dprintf(2, "Usage: %s filename text\n", av[0]);
         exit(1);
     }
-    res = create_file(av[1], av[2]);
+    res = append_text_to_file(av[1], av[2]);
     printf("-> %i)\n", res);
     return (0);
 }
