6973b9e3dc517fb19185bcd9d60c5ba9c0ff367e
Abderrahmane369 <cszamasu@gmail.com>
2023-09-15 01:18:28 +0100

    0x17

---
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - lopy
+ * @head: lop
+ * @index: lol
+ * Return: true
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	dlistint_t *c = *head;
+	unsigned int _ = 0;
+
+	while (c)
+	{
+		if (index == _)
+		{
+			if (c->prev)
+				c->prev->next = c->next;
+			else
+				*head = c->next;
+
+			if (c->next)
+				c->next->prev = c->prev;
+
+			free(c), c = NULL;
+
+			return (1);
+		}
+	}
+
+	return (-1);
+}
