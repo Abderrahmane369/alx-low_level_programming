9a5ccb8b9b62020d75f15ac683a178ef16babf40
Abderrahmane369 <cszamasu@gmail.com>
2023-10-05 20:30:41 +0100

    0x1A

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - aaaaa
+ * @size: ezis
+ * Return: None
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *ht = malloc(sizeof(hash_node_t) * size);
+
+	if (!ht)
+		return (NULL);
+
+	return (ht);
+}
