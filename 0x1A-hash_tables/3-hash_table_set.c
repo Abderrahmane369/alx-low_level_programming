e2c75e570058c552ef2cd8063ca197bf8ce6b5ae
Abderrahmane369 <cszamasu@gmail.com>
2023-10-05 22:08:32 +0100

    0x1A

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -22,7 +22,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		return (0);
 
 	new_Node->key = key;
-	new_Node->value = strdup(value);
+	new_Node->value = value;
 
 	new_Node->next = ht->array[_];
 
