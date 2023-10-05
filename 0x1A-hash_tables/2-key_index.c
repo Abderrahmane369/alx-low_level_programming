970755f1a4e27bf68c8050a70f1db7bf2d86b79d
Abderrahmane369 <cszamasu@gmail.com>
2023-10-05 21:24:06 +0100

    0x1A

--- 0x1A-hash_tables/2-key_index.c
+++ 0x1A-hash_tables/2-key_index.c
@@ -5,9 +5,9 @@
  * @key: azeae
  * @size: zeaeaze
  * Return: reee
-*/
+ */
 
 unsigned long int key_index(const unsigned char *key, unsigned long int size)
 {
-    return (hash_djb2(key) % size);
+	return (hash_djb2(key) % size);
 }
