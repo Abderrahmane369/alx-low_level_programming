06547ba0132a9e906e3d6155f7c8b97aaa370883
Abderrahmane369 <cszamasu@gmail.com>
2023-10-05 22:05:37 +0100

    0x1A

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,33 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - ttttt
+ * @ht: ht
+ * @key: k
+ * @value: bbbb
+ * Return: i
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new_Node;
+	unsigned int _ = key_index(key, ht->size);
+
+	if (!key)
+		return (0);
+
+	new_Node = malloc(sizeof(hash_node_t));
+
+	if (!new_Node)
+		return (0);
+
+	new_Node->key = key;
+	new_Node->value = strdup(value);
+
+	new_Node->next = ht->array[_];
+
+	ht->array[_]->next = NULL;
+	ht->array[_] = new_Node;
+
+	return (1);
+}
