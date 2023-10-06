2d6228ec2e936f8f35a16833e1e5d31eec39f9db
Abderrahmane369 <cszamasu@gmail.com>
2023-10-06 01:03:49 +0100

    0x1A

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -29,7 +29,6 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	{
 		if (strcmp(key, ht->array[_]->key) == 0)
 		{
-			free(ht->array[_]->value);
 			ht->array[_]->value = strdup(value);
 
 			return (1);
