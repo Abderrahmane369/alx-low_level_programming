ad1ba13a56343597ec9bf3d6858e66d3e14351b7
Abderrahmane369 <cszamasu@gmail.com>
2023-10-07 01:47:08 +0100

    0x1A

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,35 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - ttttt
+ * @ht: ht
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned int _ = 0;
+	hash_node_t *c, *n;
+
+	if (!ht)
+		return;
+
+	while (ht->size > _)
+	{
+		c = ht->array[_];
+		n = c;
+
+		while (c)
+		{
+			n = n->next;
+			free(c->key);
+			free(c->value);
+			free(c);
+			c = n;
+		}
+
+		_++;
+	}
+
+	free(ht->array);
+	free(ht);
+}
