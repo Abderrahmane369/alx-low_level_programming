b0180f8454c63cf7d104bda67d03d6890f24333a
Abderrahmane369 <cszamasu@gmail.com>
2023-11-26 23:24:38 +0100

    Makefiles

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -5,23 +5,15 @@
 def island_perimeter(grid):
     """ISLAND"""
     g = grid
-    w = 0
-    h = 0
+    w = [0 for _ in range(len(g[0]))]
+    h = [0 for _ in range(len(g))]
 
-    for i, _ in enumerate(g):
-        a = [0 for _ in range(len(g))]
-
-        for j, __ in enumerate(_):
-            a[i] = g[i][j] | a[i]
-
-        h = sum(a)
-
+    for i in range(len(g)):
+        for j in range(len(g[0])):
+            h[i] = h[i] | g[i][j]
+    
     for i in range(len(g[0])):
-        a = [0 for _ in range(len(g[0]))]
-
         for j in range(len(g)):
-            a[i] = g[j][i] | a[i]
-
-        w = sum(a)
-
-    return (w + h) * 2 
+            w[i] = w[i] | g[j][i]
+    
+    return (sum(w) + sum(h)) * 2
