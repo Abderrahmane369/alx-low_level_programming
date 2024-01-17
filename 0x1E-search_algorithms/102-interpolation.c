a45615439d0c355fe1610a5e9edff659a1c8ebc9
Abderrahmane369 <cszamasu@gmail.com>
2024-01-17 23:41:55 +0100

    Updated_Something

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -16,6 +16,9 @@ int interpolation_search(int *array, size_t size, int value)
 						 (array[high] - array[low])) *
 						(value - array[low]));
 
+	if (!array)
+		return (-1);
+
 	while (value >= array[low] && value <= array[high] && low <= high)
 	{
 		pos = low + (((double)(high - low) /
