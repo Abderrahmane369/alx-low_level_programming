fd36f3e1fc258e5b69c03bdb4dae0a270eea8606
Abderrahmane369 <cszamasu@gmail.com>
2024-01-17 01:00:28 +0100

    Updated_Something

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -6,26 +6,26 @@
  * @arr: azezae
  * @l: azeaze
  * @r: ezaeaz
-*/
+ */
 
 void printar(int *arr, size_t l, size_t r)
 {
-    size_t i;
+	size_t i;
 
-    if (l == r)
-        return;
+	if (l == r)
+		return;
 
-    printf("Searching in array: ");
+	printf("Searching in array: ");
 
-    for (i = l; i < r; i++)
-    {
-        printf("%d", arr[i]);
+	for (i = l; i < r; i++)
+	{
+		printf("%d", arr[i]);
 
-        if (i < r - 1)
-            printf(", ");
-        else
-            printf("\n");
-    }
+		if (i < r - 1)
+			printf(", ");
+		else
+			printf("\n");
+	}
 }
 
 /**
@@ -38,30 +38,30 @@ void printar(int *arr, size_t l, size_t r)
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t l = 0, r = size - 1, m;
+	size_t l = 0, r = size - 1, m;
 
-    if (!array)
-        return (-1);
+	if (!array)
+		return (-1);
 
-    printar(array, 0, size);
+	printar(array, 0, size);
 
-    while (l <= r)
-    {
-        m = floor((l + r) / 2);
+	while (l <= r)
+	{
+		m = floor((l + r) / 2);
 
-        if (array[m] < value)
-        {
-            l = m + 1;
-            printar(array, l, r + 1);
-        }
-        else if (array[m] > value)
-        {
-            r = m - 1;
-            printar(array, l, r + 1);
-        }
-        else
-            return (m);
-    }
+		if (array[m] < value)
+		{
+			l = m + 1;
+			printar(array, l, r + 1);
+		}
+		else if (array[m] > value)
+		{
+			r = m - 1;
+			printar(array, l, r + 1);
+		}
+		else
+			return (m);
+	}
 
-    return (-1);
+	return (-1);
 }
